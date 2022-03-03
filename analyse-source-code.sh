eada793e57ebe7cca0b08ceb338ca270aae42c38
Volodymyr Yahello <vyahello@gmail.com>
2022-03-03 19:04:40 +0200

    Create project skeleton

---
+++ analyse-source-code.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# specifies a set of variables to declare files to be used for code assessment
+PACKAGE="detector"
+
+# specifies a set of variables to declare CLI output color
+FAILED_OUT="\033[0;31m"
+PASSED_OUT="\033[0;32m"
+NONE_OUT="\033[0m"
+
+
+pretty-printer-box() {
+:<<DOC
+    Provides pretty-printer check box
+DOC
+    echo "Start ${1} analysis ..."
+}
+
+
+check-black() {
+:<<DOC
+    Runs "black" code analyser
+DOC
+    pretty-printer-box "black" && ( black --check ${PACKAGE} )
+}
+
+
+check-flake() {
+:<<DOC
+    Runs "flake8" code analysers
+DOC
+    pretty-printer-box "flake" && ( flake8 ${PACKAGE} )
+}
+
+
+main() {
+:<<DOC
+    Runs "main" code analyser
+DOC
+    check-black && check-flake
+}
+
+main
